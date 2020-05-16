f43d7f73b2ae271643abdab98ae6ca27dc9244e0
alinebrito <nbrito.aline@gmail.com>
2020-05-16 17:45:50 -0300

    Initial commit

---
+++ src/file1.js
@@ -0,0 +1,15 @@
+class C1{
+
+  calc(){
+      var x1 = 10
+      var x2 = 20
+      var x3 = 30
+      var result = x1 + x2 + x3
+      return result
+  }
+
+  log(msg){
+    console.log(msg)
+  }
+
+}
---
+++ src/file2.js
@@ -0,0 +1,11 @@
+class C2{
+
+  calc(){
+      var a = 100
+      var b = 200
+      var c = 300
+      var result = a - b - c
+      return result
+  }
+
+}
