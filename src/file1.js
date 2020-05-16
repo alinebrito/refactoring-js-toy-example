ed4659b585dbd29661c6f0b2ecfe40f016b2d278
alinebrito <nbrito.aline@gmail.com>
2020-05-16 17:50:09 -0300

    Extract sum() from calc()

--- src/file1.js
+++ src/file1.js
@@ -1,10 +1,13 @@
 class C1{
 
+  sum(){
+    var x1 = 10
+    var x2 = 20
+    var x3 = 30
+    return x1 + x2 + x3
+  }
   calc(){
-      var x1 = 10
-      var x2 = 20
-      var x3 = 30
-      var result = x1 + x2 + x3
+      var result = sum()
       return result
   }
 
